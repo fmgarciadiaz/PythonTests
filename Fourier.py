d094119128cb14b93849774806d54fda76026844
Fernando García Díaz <fmgarciadiaz78@gmail.com>
2025-01-30 17:39:47 -0300

    OK

--- Fourier.py
+++ Fourier.py
@@ -6,8 +6,8 @@ import librosa.display
 import matplotlib.pyplot as plt
 import numpy as np
 #%% Celda
-y, sr = librosa.load(librosa.util.example_audio_file())
-plt.figure(figsize=(12, 8))
+y, sr = librosa.load(librosa.util.example('brahms'))
+plt.figure(figsize=(12, 10))
 #%%
 D = librosa.amplitude_to_db(librosa.stft(y), ref=np.max)
 #%%
@@ -16,9 +16,4 @@ librosa.display.specshow(D, y_axis='linear')
 plt.colorbar(format='%+2.0f dB')
 plt.title('Linear-frequency power spectrogram')
 plt.show()
-
-
-# %%
-
-
 # %%
