2e9b39882950e9f97d928a509b5ffb35bad65761
Fernando Garcia Diaz <fmgarciadiaz78@gmail.com>
2025-02-03 10:31:14 -0300

    OK

--- Fourier.py
+++ Fourier.py
@@ -1,24 +1,23 @@
 # Feature extraction example
 #%%
-print("fer") 
 import librosa
 import librosa.display
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
+
 #%% Celda
-y, sr = librosa.load(librosa.util.example_audio_file())
+# Cargar un archivo de ejemplo actualizado
+audio_path = librosa.example("trumpet")
+y, sr = librosa.load(audio_path)
 plt.figure(figsize=(12, 8))
+
 #%%
 D = librosa.amplitude_to_db(librosa.stft(y), ref=np.max)
+
 #%%
 plt.subplot(4, 2, 1)
-librosa.display.specshow(D, y_axis='linear')
+librosa.display.specshow(D, y_axis='linear', x_axis='time', sr=sr)
 plt.colorbar(format='%+2.0f dB')
 plt.title('Linear-frequency power spectrogram')
 plt.show()
-
-
-# %%
-
-
 # %%
